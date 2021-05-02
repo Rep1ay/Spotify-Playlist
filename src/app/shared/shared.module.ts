dc00688029e90fb0461d5338b76e47965ddd202b
Rep1ay <90spirit8@gmail.com>
2021-05-03 00:50:05 +0300

    feat: flexlayout sidenav content

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
 
-
+import { FlexLayoutModule } from '@angular/flex-layout'
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    FlexLayoutModule
+  ],
+  exports: [
+    MatSidenavModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule
   ]
+
 })
 export class SharedModule { }
