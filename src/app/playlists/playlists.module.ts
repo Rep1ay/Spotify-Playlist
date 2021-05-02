5316063794118f70dd88eedd4e72140479dc7f3f
Rep1ay <90spirit8@gmail.com>
2021-05-03 00:28:55 +0300

    feat: add shared module |&| music search component

--- src/app/playlists/playlists.module.ts
+++ src/app/playlists/playlists.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlaylistsComponent } from './playlists.component';
+import { PlaylistsRoutingModule } from './playlists-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -8,7 +10,9 @@ import { PlaylistsComponent } from './playlists.component';
     PlaylistsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    PlaylistsRoutingModule,
+    SharedModule
   ]
 })
 export class PlaylistsModule { }
